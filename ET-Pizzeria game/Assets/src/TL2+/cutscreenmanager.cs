6270d44ede4b9fdc7a311355fa5e8e549136ea95
samwould <samuilwould@gmail.com>
2026-03-03 14:41:58 -0800

    Update

--- ET-Pizzeria game/Assets/src/TL2+/cutscreenmanager.cs
+++ ET-Pizzeria game/Assets/src/TL2+/cutscreenmanager.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class cutscreenmanager : MonoBehaviour
 {
+    private Vector2 startpos;
+    private Vector2 endpos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnMouseDown()
     {
-        
+        startpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Update is called once per frame
-    void Update()
+    void OnMouseUp()
     {
-        
+        endpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        performcut(startpos, endpos);
+    }
+
+    void performcut(Vector2 start, Vector2 end)
+    {
+        Debug.Log("Cut from " + start + " to " + end);
     }
 }
