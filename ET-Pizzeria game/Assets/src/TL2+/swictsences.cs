acc4075f158e4cc6dee591fb7db5b466926e3479
samwould <samuilwould@gmail.com>
2026-03-04 19:48:13 -0800

    UpdatetoUnlyGUI
    It still work and still Ugly

---
+++ ET-Pizzeria game/Assets/src/TL2+/swictsences.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+
+public class swictsences : MonoBehaviour
+{
+    public void LoadSpecificScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
