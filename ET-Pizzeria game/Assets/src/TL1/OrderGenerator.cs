e916ef72315fd4c0d40fe95c16cfa811f0437aa2
ZuziaWhit <wieczorekzuzanna2@gmail.com>
2026-03-08 19:13:59 -0700

    Version 0.1 of OrderGenerator. Implemented changes: Ingredients list, slice types list, and cook time list. RandomOrderGenerator class successfully fills in a PizzaOrder public object, which contains 3 randomly selected ingredients, 1 randomly selected slice type, and 1 randomly selected cook time. The startup function logs these values into the Console if this script is associated with a Game Object. Tested on a temporary diamond in the Scene, works successfully.

--- ET-Pizzeria game/Assets/src/TL1/OrderGenerator.cs
+++ ET-Pizzeria game/Assets/src/TL1/OrderGenerator.cs
@@ -1,9 +1,129 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class OrderGenerator
+public class PizzaOrderGenerator : MonoBehaviour
 {
-    void Start()
+
+    /*
+     -------------------------
+     ORDER DATABASE
+     -------------------------
+     Unity 6 best practice:
+     Keep order data separate so it can be edited later.
+    */
+
+    /*
+    The ingredientDatabase array contains the different ingredients.
+    */
+    public string[] ingredientDatabase =
     {
-        Debug.Log("Pizza system initialized.");
+        "Pepperoni",
+        "Mushroom",
+        "Onion",
+        "Sausage",
+        "Bacon",
+        "ExtraCheese",
+        "BlackOlives",
+        "GreenPeppers"
+    };
+
+    /*
+    The cutTypes array contains the amount of slices the pizza will be cut into.
+    */
+    public string[] cutTypes =
+    {
+        "1",
+        "2",
+        "4"
+    };
+
+    /*
+    The bakeTimes array contains the different baking times in units of seconds.
+    */
+    public string[] bakeTimes =
+    {
+        "5s",
+        "10s",
+        "15s"
+    };
+
+
+    /*
+     -------------------------
+     ORDER OBJECT
+     -------------------------
+     Serializable so Unity can
+     display it in the Inspector.
+    */
+
+    [System.Serializable]
+    public class PizzaOrder
+    {
+        public string ingredient1;
+        public string ingredient2;
+        public string ingredient3;
+        public string cutType;
+        public string bakeTime;
     }
+
+
+    /*
+     -------------------------
+     RANDOM ORDER GENERATOR
+     -------------------------
+    */
+
+    public PizzaOrder GenerateRandomOrder()
+    {
+        PizzaOrder order = new PizzaOrder();
+
+        HashSet<int> selectedIngredients = new HashSet<int>();
+
+        // Ensure we get 3 unique ingredients
+        while (selectedIngredients.Count < 3)
+        {
+            int randomIndex = Random.Range(0, ingredientDatabase.Length);
+            selectedIngredients.Add(randomIndex);
+        }
+
+        int i = 0;
+
+        foreach (int index in selectedIngredients)
+        {
+            if (i == 0)
+                order.ingredient1 = ingredientDatabase[index];
+            else if (i == 1)
+                order.ingredient2 = ingredientDatabase[index];
+            else if (i == 2)
+                order.ingredient3 = ingredientDatabase[index];
+
+            i++;
+        }
+
+        order.cutType = cutTypes[Random.Range(0, cutTypes.Length)];
+        order.bakeTime = bakeTimes[Random.Range(0, bakeTimes.Length)];
+
+        return order;
+    }
+
+
+    /*
+     -------------------------
+     UNITY INTEGRATION
+     -------------------------
+    */
+
+    private void Start()
+    {
+        PizzaOrder order = GenerateRandomOrder();
+
+        Debug.Log("New Pizza Order Generated/n");
+
+        Debug.Log("Ingredient 1: " + order.ingredient1);
+        Debug.Log("Ingredient 2: " + order.ingredient2);
+        Debug.Log("Ingredient 3: " + order.ingredient3);
+        Debug.Log("Cut Type: " + order.cutType);
+        Debug.Log("Bake Time: " + order.bakeTime);
+    }
+
 }
