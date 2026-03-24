215ae59ca0f1d5d2423230e4d649d05b112b93ad
TanZanite21 <emily.meg.thomas@gmail.com>
2026-03-23 23:38:21 -0700

    My TL2 Test Cases that don't work
    I just want to save it somewhere

---
+++ ET-Pizzeria game/Assets/src/TL2/tmp/SimpleTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+public class SimpleTest
+{
+    [Test]
+    public void AlwaysPasses()
+    {
+        Assert.IsTrue(true);
+    }
+}
+
---
+++ ET-Pizzeria game/Assets/tst/TL2/TL2Tests/ScoreBoundaryTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.TestRunner;
+
+
+public class ScoreBoundaryTest
+{
+    [Test]
+    public void Score_Clamps_Values_Outside_Valid_Range()
+    {
+        // Create a GameObject so we can attach the controller
+        GameObject go = new GameObject();
+        ToppingController controller = go.AddComponent<ToppingController>();
+
+        // ---- Just outside the bounds ----
+        Assert.AreEqual(100, controller.SetScore(105));
+        Assert.AreEqual(0, controller.SetScore(-1));
+        Assert.AreEqual(0, controller.SetScore(-5));
+        Assert.AreEqual(100, controller.SetScore(110));
+
+        // ---- Extremely outside the bounds ----
+        Assert.AreEqual(0, controller.SetScore(-100));
+        Assert.AreEqual(100, controller.SetScore(300));
+        Assert.AreEqual(0, controller.SetScore(-11111));
+        Assert.AreEqual(100, controller.SetScore(11111));
+    }
+}
