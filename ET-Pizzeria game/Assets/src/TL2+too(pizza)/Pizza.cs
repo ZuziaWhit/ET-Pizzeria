75611eb2ac6b9056075756a37ed84190b35e51d6
samwould <samuilwould@gmail.com>
2026-03-28 10:48:50 -0700

    Big pizza update (forgot to save so stuff)

--- ET-Pizzeria game/Assets/src/TL2+too(pizza)/Pizza.cs
+++ ET-Pizzeria game/Assets/src/TL2+too(pizza)/Pizza.cs
@@ -8,18 +8,21 @@ public class Pizza : MonoBehaviour
     public float bakingTime = 0f;
     public int numberCuts = 0;
 
+    //This is to be use by the Baking Station
     public void AddBakeTime(float baketime)
         {
             bakingTime = baketime;
             Debug.Log("Pizza is baking" + bakingTime);
         }
 
+    //This is to be use by the Topping Station
     public void AddIngredient(string ingredient)
     {
         ingredients.Add(ingredient);
         Debug.Log("Added: " + ingredient);
     }
 
+    //This is to be use by the Cutting Station
     public void AddCut()
     {
         numberCuts++;
