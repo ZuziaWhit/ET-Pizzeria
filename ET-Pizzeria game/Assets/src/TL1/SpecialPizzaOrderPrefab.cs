dd709ad13c2e1f1dcf6b6d0abc0a37db687cfb80
ZuziaWhit <wieczorekzuzanna2@gmail.com>
2026-04-18 20:00:12 -0700

    Update SpecialPizzaOrderPrefab.cs

--- ET-Pizzeria game/Assets/src/TL1/SpecialPizzaOrderPrefab.cs
+++ ET-Pizzeria game/Assets/src/TL1/SpecialPizzaOrderPrefab.cs
@@ -64,3 +64,14 @@ public class SpecialPizzaOrderPrefab : PizzaOrderPrefab
         cutText.text = "✂ Cut Type: " + orderData.cutType;
     }
 }
+
+
+// -------------------------- Change Prefab ----------------------
+
+//In your PizzaOrderManager:
+
+//A ssign it:
+
+/*
+public GameObject pizzaOrderPrefab; // drag SPECIAL prefab here in Inspector
+*/
